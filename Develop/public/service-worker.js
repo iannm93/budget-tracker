a4e38c0918291f83d8f728f2eb5b500464709a73
ianm93 <45110918+iannm93@users.noreply.github.com>
2020-05-04 12:59:13 -0700

    changed manifest to .webmanifest instead of .json, added service worker boiler plate, made web manifest

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -4,3 +4,79 @@
 // when the app is used offline. HINT: You should use two caches. One for the
 // static assets such ass html, css, js, images, etc; and another cache for
 // the dynamic data from requests to routes beginning with "/api".
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/styles.css",
+    "/favicon.ico",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // function that listen for an install then creates a cache
+  self.addEventListener("install", (evt) => {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        // adds everyting from created cache to files_to_cache
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (evt) => {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // event listener for a fetch
+  self.addEventListener("fetch", (evt) => {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(() => {
+                return cache.match(evt.request);
+                // catches an error if it occurs
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+      return;
+    }
+
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
